a33d244bbfa3bb65fbcba7cec8bd3cd62ade9781
Khurram <syed.arbaaz.khurram@accenture.com>
2021-09-25 15:09:20 +0530

    Box component optimized

--- App.js
+++ App.js
@@ -4,9 +4,8 @@ import { Button, FlatList, LogBox, Platform, SafeAreaView, StyleSheet, Text, Vie
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Constants from 'expo-constants';
 import firebase from "@firebase/app";
-import "@firebase/auth";
+import "@firebase/storage";
 import "@firebase/firestore";
-
 //you have to hide these vals., see the liked tweet
 if (!firebase.apps.length) {
   firebase.initializeApp({
@@ -25,43 +24,37 @@ export default function App() {
   LogBox.ignoreLogs(['Setting a timer']);
   const [data, setData] = useState([]);
   const [limit, setLimit] = useState(null);
-  
+
   //to make sure we dont sotre limit twice when from top is presssed
   const [fromTop, setFromTop] = useState(false);
   //color palette for text boxes
-  const colors = [`#90ee90`, `#e0ffff`, `#7fffd4`, `#f0f8ff`, `#afeeee`, `#00ff7f`, `#40e0d0`, `#ffc0cb`];
+  // const colors = [`#90ee90`, `#e0ffff`, `#7fffd4`, `#f0f8ff`, `#afeeee`, `#00ff7f`, `#40e0d0`, `#ffc0cb`];
   // for now we are using global variable
   let x = [];
   let flatlist;
   //we can write {item} here and HAVE TO use item.attr_name inside 
-  const Box = ({ item }) => (
-    <View style={{ padding: 4 }}>
-      {(parseInt(item.id) <= limit)  ? //using ternary inside a ternary oprtr.
-        (parseInt(item.id) % 2 == 0) ?
-          <View style={{ alignSelf: "flex-start", maxWidth: "90%", backgroundColor: colors[parseInt(item.id) % 8], borderRadius: 10, marginLeft: 7, padding: 10 }}>
-            <Text style={{ fontWeight: "bold" }}>
-              {item.name}
-            </Text>
-            <Text >
-              {item.id}
-              {item.words}
-            </Text>
-          </View>
-          :
-          <View style={{ alignSelf: "flex-end", maxWidth: "90%", backgroundColor: colors[parseInt(item.id) % 8], borderRadius: 10, marginRight: 7, padding: 10 }}>
-            <Text style={{ fontWeight: "bold" }}>
-              {item.name}
-            </Text>
-            <Text>
-              {item.id}
-              {item.words}
-            </Text>
-          </View>
-        :
-        null
-      }
-    </View>
-  )
+  const Box = ({ item }) => {
+    const colors = [`#90ee90`, `#e0ffff`, `#7fffd4`, `#f0f8ff`, `#afeeee`, `#00ff7f`, `#40e0d0`, `#ffc0cb`];
+    const align = ["flex-start", "flex-end"];
+    const margin = [7, 0];
+    return (
+        <View style={{ padding: 4 }}>
+            {(parseInt(item.id) <= limit) ? //using ternary inside a ternary oprtr
+                <View style={{ alignSelf: align[parseInt(item.id) % 2], maxWidth: "90%", backgroundColor: colors[parseInt(item.id) % 8], borderRadius: 10, marginLeft: margin[parseInt(item.id) % 2], marginRight: margin[parseInt(item.id) % 2], padding: 10 }}>
+                    <Text style={{ fontWeight: "bold" }}>
+                        {item.name}
+                    </Text>
+                    <Text >
+                        {item.id}
+                        {item.words}
+                    </Text>
+                </View>
+                :
+                null
+            }
+        </View>
+    );
+};
 
   //key is the name of the doc in firestore
   const storeData = async (key, value) => {
@@ -91,15 +84,11 @@ export default function App() {
             let words = temp2[1];
             num += 1;
             let id = num.toString();
-            for(let i=0; i<30; ++i){
             x.push({
               id,
               name,
               words
             });
-            num+=1;
-             id = num.toString();
-          }
           }
           setData(x);
         }
@@ -113,6 +102,7 @@ export default function App() {
 
   const addData = async () => {
     console.log("in add Data");
+
     try {
       let n = await AsyncStorage.getItem("harrypotter_start");
       if (n != null) setLimit(parseInt(n) + 5);
@@ -126,8 +116,6 @@ export default function App() {
         console.log("storing limit");
         if (!fromTop) storeData("harrypotter_start", (limit).toString());
         else setFromTop(false);
-        
-       
       }
     }
     return;
@@ -147,17 +135,24 @@ export default function App() {
       //data might show empty as we are using async functions
       //else console.log("cant find books || local data -----> " + data);
     });
+    //work in progress for cloud storage, accesible by both, google storage and firebase storage
+    const storage = firebase.storage();
   }, []);
 
-  const restart = () => {
-    storeData("harrypotter_start", "5");
-    setFromTop(true);
-  }
+  // const restart = () => {
+  //   storeData("harrypotter_start", "5");
+  //   setFromTop(true);
+  //   goToTop();
+  // }
 
-  const goToTop = () => (flatlist.scrollToOffset({offset:0, animated:true}))
+  const goToTop = () => (flatlist.scrollToOffset({ offset: 0, animated: true }))
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.container}>
+        <Text style={{ alignSelf: "center", backgroundColor: "yellow", fontWeight: "bold", fontSize: 20 }}>
+          Harry Potter and the sorcerer's stone
+        </Text>
         <FlatList
           data={data}
           initialNumToRender={3}
@@ -165,13 +160,10 @@ export default function App() {
           ref={ref => flatlist = ref}
           renderItem={Box}
         />
-        <View>
+        <View style={{ justifyContent: "center", flexDirection: "row", padding: 10 }}>
           <Button title="Press x2"
             onPress={addData} />
-          <Button title="from start ?"
-            onPress={restart}
-            color={"red"} />
-            <Button title="top" onPress={goToTop}/>
+          <Button title="top" onPress={goToTop} />
         </View>
       </View>
     </SafeAreaView>
@@ -180,7 +172,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
+    backgroundColor: `#fffaf0`,
     flex: 1,
     justifyContent: 'center',
     marginTop: Platform.OS === "android" ? 40 : 0,
