f27e9e0b431f1b7366353a75e64c5fbb8d0ccb8c
Khurram <syed.arbaaz.khurram@accenture.com>
2021-10-09 18:53:57 +0530

    new look

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { Button, FlatList, LogBox, Platform, SafeAreaView, StyleSheet, Text, Vie
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {test_screen_1, test_screen_2} from "./app/screens";
+import {chats, test_screen_1, test_screen_2} from "./app/screens";
 import Constants from 'expo-constants';
 import firebase from "@firebase/app";
 import "@firebase/storage";
@@ -37,6 +37,7 @@ export default App = () => {
     <SafeAreaView style={{ flex: 1 }}>
       <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name ="chats" component={chats} />
         <Stack.Screen name="screen_2" component={test_screen_2} />
         <Stack.Screen name="screen_1" component={test_screen_1} />
       </Stack.Navigator>
