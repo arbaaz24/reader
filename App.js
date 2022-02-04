2b7b48e0c6fddff0f0967ecd971e5b078cf80b92
Khurram <syed.arbaaz.khurram@accenture.com>
2022-02-05 02:13:58 +0530

    limit code optimised

--- App.js
+++ App.js
@@ -1,79 +1,60 @@
 //this edit is only visible on slave branch
 import React, { useEffect, useState } from 'react'
-import {  LogBox, Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import AsyncStorage from "@react-native-async-storage/async-storage"
+import { LogBox, Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { chats, main, store, myaccount, subscribed } from "./app/screens"
-import Constants from 'expo-constants'
-import firebase from "@firebase/app"
-import "@firebase/storage"
-import "@firebase/firestore"
-import "@firebase/auth"
+import { chats, main, store, myaccount, subscribed, login } from "./app/screens"
+import { initializeApp } from 'firebase/app'
+import { doc, getDoc, getFirestore, query } from "firebase/firestore"
+import { getAuth, onAuthStateChanged } from "firebase/auth"
+import { firebaseConfig } from "./app/components/firebaseConfig"
 
-if (!firebase.apps.length) {
-  firebase.initializeApp({
-    apiKey: Constants.manifest.extra.apiKey,
-    authDomain: Constants.manifest.extra.authDomain,
-    databaseURL: Constants.manifest.extra.datatbaseURL,
-    projectId: Constants.manifest.extra.projectId,
-    storageBucket: Constants.manifest.extra.storageBucket,
-    messagingSenderId: Constants.manifest.extra.messagingSenderId,
-    appId: Constants.manifest.extra.appId,
-    measurementId: Constants.manifest.extra.measurementId
-  })
-}
-
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
 const Stack = createNativeStackNavigator()
+const db = getFirestore(app)
+
 export default App = () => {
-  // usePreventScreenCapture()
   LogBox.ignoreLogs(['Setting a timer'])
   const [user, setUser] = useState(null)
 
-useEffect(() => {
-  console.log("************************ WORKING *************************")
-  const usersRef = firebase.firestore().collection("users")
-  firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      usersRef
-        .doc(user.uid)
-        .get()
-        .then((document) => {
-          const userData = document.data()
-          setLoading(false)
-          setUser(userData)
-        })
-        .catch((error) => {
-          console.log("error in useEffect() of App.js")
-          //setLoading(false)
-        })
+  const getUserDoc = async (userData) => {
+    const snap = await getDoc(doc(db, 'users', userData.uid))
+    if (snap.exists()) {
+      //console.log("in App.js , snap-->", snap.data())
+      setUser(snap.data())
     }
-  })
-}, [])
+  }
+
+  useEffect(() => {
+    console.log("************************ WORKING *************************")
+    onAuthStateChanged(auth, userData => {
+      if (userData)
+        if (userData.emailVerified) getUserDoc(userData)
+        else alert("Please complete verification sent to your email.")
+    })
+  }, [])
 
 
-return (
-  <SafeAreaView style={styles.container}>
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false, cardShadowEnabled:true, gestureEnabled: true}}>
-        {!user ?
-        <>
-        <Stack.Screen name="main" component={main}/>
-        <Stack.Screen name="store" component={store}/>
-        <Stack.Screen name="subscribed" component={subscribed} />
-        <Stack.Screen name="myaccount" component={myaccount} /> 
-        <Stack.Screen name="chats" component={chats} /> 
-        </>
-        :
-        <>
-        <Stack.Screen name="myaccount" component={myaccount} />
-        </>
-        }
-         
-      </Stack.Navigator>
-    </NavigationContainer>
+  return (
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false, cardShadowEnabled: true, gestureEnabled: true }}>
+          {user ?
+            <>
+              {/* <Stack.Screen name="main" component={main} /> */}
+              <Stack.Screen name="store" component={store} initialParams={{user}} />
+              <Stack.Screen name="subscribed" component={subscribed} initialParams={{user}} />
+              <Stack.Screen name="chats" component={chats} initialParams={{user}} />
+              <Stack.Screen name="myaccount" component={myaccount} initialParams={{user}} />
+            </>
+            :
+            <Stack.Screen name="login" component={login} />
+          }
+        </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaView>
-)
+  )
 }
 
 const styles = StyleSheet.create({
--- app/screens/chats.js
+++ app/screens/chats.js
@@ -1,142 +1,154 @@
-import React, { useEffect, useState } from 'react';
-import { FlatList, Platform, Pressable, SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import Icon from 'react-native-vector-icons/Ionicons';
-import IconMaterial from 'react-native-vector-icons/MaterialCommunityIcons';
-
-// import Constants from 'expo-constants';
-import firebase from "@firebase/app";
-import "@firebase/storage";
-import "@firebase/firestore";
+import React, { useEffect, useState } from 'react'
+import { FlatList, Platform, Pressable, SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import Icon from 'react-native-vector-icons/Ionicons'
+import IconMaterial from 'react-native-vector-icons/MaterialCommunityIcons'
+import { initializeApp } from "firebase/app"
 import Box from "../components/Box.js"
-export default chats = ({ navigation, props, route }) => {
-  //prevents screen shots
- 
-  const { screenName } = route.params
+import { getFirestore, getDoc, doc, setDoc, updateDoc, arrayUnion } from 'firebase/firestore'
+import { firebaseConfig } from "../components/firebaseConfig"
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+export default chats = ({ navigation, route }) => {
+  const { screenName, url, user } = route.params //url is the GCloud storage url for clicked image
   const screenStart = screenName + "_start"
   const [data, setData] = useState("")
   const [limit, setLimit] = useState(null)
+  const [subscribed, setSubscribed] = useState(false)
   //to make sure we dont sotre limit twice when from top is presssed
   const [fromTop, setFromTop] = useState(false);
   // for now we are using global variable
   let x = []
   let mainString = ""
   let flatlist
-  
 
   //key is the name of the doc in firestore
-  const storeData = async (key, value, location) => {
-    try {
-      await AsyncStorage.setItem(key, value)
-      console.log("data stored successfully from function " + location +"\n")
-    } catch (e) {
-      console.log("error storing data" + e)
-    }
-  }
   const getData = async (key) => {
     let value
     console.log("in getData()")
     try {
       //getting data (in JSON) from  local storage
-      //can we use AsynStorage.getItem().then() ??
       value = await AsyncStorage.getItem(key)
       if (value !== null) {
         // value previously stored
         // console.log(value);
         let num = 0
-          //= is our delimiter
-          let temp = value.split('^')
-          for (let str of temp) {
-            let temp2 = str.split(">")
-            let name = temp2[0]
-            let words = temp2[1]
-            let id = num.toString()
-            num += 1
-            x.push({
-              id,
-              name,
-              words
-            });
+        //^ is our delimiter
+        let temp = value.split('^')
+        for (let str of temp) {
+          let temp2 = str.split(">")
+          let name = temp2[0]
+          let words = temp2[1]
+          let id = num.toString()
+          num += 1
+          x.push({
+            id,
+            name,
+            words
+          });
           setData(x);
         }
         // console.log("x looks like this ", x);
       }
-      else{//here downloadData() because we want to download only if not available locally
+      else {//here downloadData() because we want to download only if not available locally
         downloadData()
-      } 
+      }
     } catch (e) {
       console.log("No data in local storage " + e);
-    }// finally{
-    //   x=[]
-    // }
-
+    }
   }
-  const addData = async () => {
-    console.log("in add Data");
+  
+  const storeData = async (key, value, location) => {
     try {
+      await AsyncStorage.setItem(key, value)
+      console.log("data stored successfully from function " + location + "\n")
+    } catch (e) {
+      console.log("error storing data" + e)
+    }
+  }
+
+  const addData = async () => {
+    console.log("in addData()");
+
       let n = await AsyncStorage.getItem(screenStart);
-      if (n != null) setLimit(parseInt(n) + 5);
-      console.log("limit -> " + limit);
+      if (n != null) {
+      setLimit(parseInt(n) + 5)
+      if (!fromTop) storeData(screenStart, (limit === null ? "5" :(limit).toString()), "addData()");
+      else setFromTop(false);
     }
-    catch (e) {
-      console.log("data not found " + e);
-    }
-    finally {
-      if (limit != null) {
-        if (!fromTop) storeData(screenStart, (limit).toString(), "addData()");
-        else setFromTop(false);
-      }
-    }
+    else storeData(screenStart, "0", "restart()")
+    console.log("limit -> " + limit);
+    
     return;
   }
-  const restart = () => {
+
+  const restart = async () => {
     storeData(screenStart, "0", "restart()")
     setFromTop(true)
-    
     goToTop()
   }
 
-  const goToTop = () => { flatlist.scrollToOffset({ offset: 0, animated: true }) }
-  
+  const goToTop = () =>  flatlist.scrollToOffset({ offset: 0, animated: true }) 
 
   const downloadData = async () => {
     console.log("in downloadData()\n")
-    const db = firebase.firestore();
-    db.collection("movies").doc(screenName).get().then((doc) => {
-      //here, change mainString.length !=0 to download again, else it downloads only once
-      if (doc.exists && mainString.length == 0) {
-        mainString = doc.data().a
-        storeData(screenName, mainString, "downloadData()")
-        getData(screenName)
-
-      }
-      //data might show empty as we are using async functions
-      else console.log("cant find books || local data -----> " + data);
-    });
+    const db = getFirestore(app)
+    const snap = await getDoc(doc(db, "movies", screenName))
+    //here, change mainString.length !=0 to download again, else it downloads only once
+    if (snap.exists() && mainString.length == 0) {
+      mainString = snap.data().a
+      storeData(screenName, mainString, "downloadData()")
+      getData(screenName)
+    }
+    //data might show empty as we are using async functions
+    else console.log("cant find books || local data -----> " + data)
   }
+
   useEffect(() => {
-    console.log("************************ WORKING *************************");
+    console.log("in chats > useEffects and user.uid -> ", user.uid, "screen path is ->", route.params.path)
     getData(screenName)
-    //line under should be removed in production
-    //downloadData()
+    
   }, [])
 
-  const render =({item})=>{
-    return(
+  const render = ({ item }) => {
+    return (
       <Box limit={limit} item={item} />
     )
   }
 
-  const removeData = () =>{
-    AsyncStorage.removeItem(screenName)
+  const removeData = async () => {
+    await AsyncStorage.removeItem(screenName)
+  }
+
+  const subscribe = async () => {
+    console.log("in subscribe() and user.uid -->", user.uid)
+    updateDoc(doc(db, 'users', user.uid), { subscriptions: arrayUnion(url) })
+    setSubscribed(true)
+    let links = await AsyncStorage.getItem("subscribed" + user.uid)
+    if (links === null) {
+      await AsyncStorage.setItem("subscribed" + user.uid, JSON.stringify([url]))
+    }
+    else {
+      let links2 = JSON.parse(links)
+      //before pushing we have to make sure that the url dosen't already exist, includes("arg here is case sensitive")
+      if(links2.includes(url)) alert("Already subscribed")
+      else links2.push(url)
+      await AsyncStorage.setItem("subscribed" + user.uid, JSON.stringify(links2))
+    }
   }
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.container}>
-        <Text style={{ alignSelf: "center", backgroundColor: "yellow", fontWeight: "bold", fontSize: 20 }}>
-          {screenStart}
-        </Text>
+        <View style={{ alignSelf: "center", backgroundColor: "yellow", fontWeight: "bold", fontSize: 20, flexDirection: "row" }}>
+          <Text>{screenStart}</Text>
+          <Pressable style={{ borderRadius: 5, height: 40, width: 40, backgroundColor: subscribed ? "blue" : "red" }}
+            onPress={subscribe}>
+            <Text>{subscribed ? "Unsubscribe" : "subscribe"}</Text>
+          </Pressable>
+        </View>
         <FlatList
           data={data}
           keyExtractor={item => item.id}
@@ -152,36 +164,37 @@ export default chats = ({ navigation, props, route }) => {
             onPressIn={addData}
             onPressOut={addData}
             style={{ borderRadius: 20, height: 40, width: 40, backgroundColor: "green" }}>
-      <Icon name="arrow-down" size={30} style={{alignSelf:"center"}}  />
-      </Pressable>
-      
+            <Icon name="arrow-down" size={30} style={{ alignSelf: "center" }} />
+          </Pressable>
+
           <Pressable onPressIn={goToTop}
-          style={{ borderRadius: 20, height: 40, width: 40, backgroundColor: "orange" }}>
-            <Icon name="arrow-up" size={30} style={{alignSelf:"center"}}  />
+            style={{ borderRadius: 20, height: 40, width: 40, backgroundColor: "orange" }}>
+            <Icon name="arrow-up" size={30} style={{ alignSelf: "center" }} />
           </Pressable>
           {/*need to remove under button when in production*/}
           <Pressable
             onPressIn={restart}
             onPressOut={addData}
             style={{ borderRadius: 20, height: 40, width: 40, backgroundColor: "red" }} >
-              <IconMaterial name="delete" size={30} style={{alignSelf:"center"}}  />
-              </Pressable>
+            <IconMaterial name="delete" size={30} style={{ alignSelf: "center" }} />
+          </Pressable>
           <Pressable
             onPressIn={removeData}
             onPressOut={removeData}
             style={{ borderRadius: 20, height: 40, width: 40, backgroundColor: "blue" }} >
-              <Icon name="md-refresh-sharp" size={30} style={{alignSelf:"center"}}  />
-              </Pressable>
+            <Icon name="md-refresh-sharp" size={30} style={{ alignSelf: "center" }} />
+          </Pressable>
         </View>
       </View>
     </SafeAreaView>
   );
+
 }
 const styles = StyleSheet.create({
   container: {
     backgroundColor: `#fffaf0`,
     flex: 1,
     justifyContent: 'center',
-    marginTop: Platform.OS === "android" ? 40 : 0,
+    //marginTop: Platform.OS === "android" ? 40 : 0,
   },
 });
